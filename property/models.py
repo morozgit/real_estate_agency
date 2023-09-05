64e20f44cea8365043e2f3d744c6b35eaaafe749
morozgit <morozec76@gmail.com>
2023-09-05 20:48:56 +0300

    add related_name

--- property/models.py
+++ property/models.py
@@ -72,7 +72,8 @@ class Complaint(models.Model):
     flat = models.ForeignKey(
         Flat,
         on_delete=models.CASCADE,
-        verbose_name='Квартира, на которую пожаловались'
+        verbose_name='Квартира, на которую пожаловались',
+        related_name='complaints'
         )
     text = models.TextField(
         'Текст жалобы'
